83f9e8ab59e3b8f4565bdc2dcc30e4ff59c75962
delchev <delchevn@gmail.com>
2024-11-23 21:20:21 +0200

    model binding

--- codbex-sample-vue-tailwind/components/TextBox.vue
+++ codbex-sample-vue-tailwind/components/TextBox.vue
@@ -1,3 +1,52 @@
 <template>
-  <input type="text" class="w-full border-gray-300 rounded-lg shadow-sm focus:ring-blue-500 focus:border-blue-500 p-2">
+  <div class="mb-4">
+    <label v-if="label" :for="id" class="block text-sm font-medium text-gray-700 mb-2">{{ label }}</label>
+    <input
+      :id="id"
+      :type="type"
+      :value="modelValue"
+      :placeholder="placeholder"
+      class="w-full border-gray-300 rounded-lg shadow-sm focus:ring-blue-500 focus:border-blue-500 p-2"
+      :class="{ 'text-box-error': hasError }"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+    <small v-if="hasError" class="text-box-error-message">{{ errorMessage }}</small>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "StyledTextBox",
+  props: {
+    id: {
+      type: String,
+      required: false,
+    },
+    modelValue: {
+      type: String,
+      default: "",
+    },
+    label: {
+      type: String,
+      default: "",
+    },
+    type: {
+      type: String,
+      default: "text", // Supports text, password, etc.
+    },
+    placeholder: {
+      type: String,
+      default: "",
+    },
+    hasError: {
+      type: Boolean,
+      default: false,
+    },
+    errorMessage: {
+      type: String,
+      default: "Invalid input",
+    },
+  },
+};
+</script>
+
